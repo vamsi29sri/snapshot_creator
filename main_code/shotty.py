823ddac7c8a3aab60b59101379198e3b75fa47d0
Bhadriraju <bhadrir@dca9047db411.ant.amazon.com>
2019-05-31 15:03:56 -0700

    corrected code

--- main_code/shotty.py
+++ main_code/shotty.py
@@ -1,5 +1,5 @@
 import boto3
-if __name=='__main__':
+if __name__=='__main__':
     session =boto3.Session(profile_name ='snapshoter')
     ec2=session.resource('ec2')
     for i in ec2.instances.all():
